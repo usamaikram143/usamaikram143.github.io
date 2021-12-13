f79017127327ebe60ef9942f9bbb6364f79ea4a1
usamaikram143 <usamaikram143@gmail.com>
2021-12-12 22:27:42 -0500

    added a logo to nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,11 @@
 import React,{Component} from "react";
 import '../css/Nav.css'
 import {Link} from 'react-router-dom';
+import logo from "../logo.jpg"
 function Nav(){
     return(
         <nav>
+            <img src={logo} className="logo"/>
             <Link to="/" className="navLinks"> Home </Link>
             <Link to="/about" className="navLinks"> About </Link>
             <Link to="/projects" className="navLinks"> Projects </Link>
