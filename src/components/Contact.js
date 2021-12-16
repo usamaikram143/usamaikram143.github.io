772cb0d632351b76e9de3d19acc9c2242a67ea51
usamaikram143 <usamaikram143@gmail.com>
2021-12-15 22:13:05 -0500

    added contacts on contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,20 @@
-
+import '../css/Contact.css';
 
 const Contact=() =>{
     return(
-        <div>
-            Contact page
+        <div className="contact">
+            <div className="headline">Here are some of the ways to contact me</div>
+            <div>
+                Email Address: qasim.buttar143@gmail.com 
+            </div>
+            <div>
+                Github profile:
+                <a href="https://github.com/usamaikram143">github!</a>
+            </div>
+            <div>
+                LinkedIn profile:
+                <a href="https://www.linkedin.com/in/usama-ikram-143143usama/">LinkedIn!</a>
+            </div>
         </div>
     );
 }
